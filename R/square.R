4ea0c274fad01791933c1ef14675b21aafb6772a
Ian <icoccimi@gmail.com>
2023-10-31 10:30:35 -0700

    Added documentation

--- R/square.R
+++ R/square.R
@@ -1,3 +1,12 @@
+#' Squares an input number
+#'
+#' @param x a numeric value
+#'
+#' @return the squared value \code{x}
+#' @export
+#'
+#' @examples
+#' square(10)
 square <- function(x) {
   x^2
 }
