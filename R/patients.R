590b075e3f3f4e14edc0c3ffcf33fc3709aff3e7
Ian <icoccimi@gmail.com>
2023-11-02 10:42:08 -0700

    Initial commit

---
+++ R/patients.R
@@ -0,0 +1,12 @@
+#' Patient data
+#'
+#' @description
+#' Some patient data
+#'
+#' @format ## `patients`
+#' A data frame with 3 rows and 2 columns:
+#' \describe{
+#'   \item{id}{id f patient}
+#'   \item{age}{years of age}
+#' }
+"patients"
